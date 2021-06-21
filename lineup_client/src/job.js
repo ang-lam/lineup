2417e3c34cc5dc2eb2e4e731325bd1bb99a1c2e2
Angela <angelalamluu@gmail.com>
2021-06-21 14:52:09 -0700

    created HTML and append functions for new job row

--- lineup_client/src/job.js
+++ lineup_client/src/job.js
@@ -3,6 +3,8 @@ class Job {
     //will need to remove the elements from frontend with a method (filter and remove from array)
     //the all array will be populated/updated with all instances at a refresh
     static all = []
+    static jobContainer = document.getElementById('job-container')
+    static jobTable = document.getElementById('job-table')
 
     constructor(id, title, company, dateApplied, link, status){
         this.id = id
@@ -13,10 +15,28 @@ class Job {
         this.status = status
         //instantiate the object with own element in constructor function
         //can reference job element with this.__
-        this.element = document.createElement('table')
+        this.element = document.createElement('tr')
         this.element.dataset.id = this.id
         this.element.id =  `job-${this.id}`
         
         Job.all.push(this) //new instance pushed into all array
+    }
+
+    //function is responsible for creating the HTML in each element of an instance
+    jobHTML() {
+        this.element.innerHTML = `
+            <td>${this.title}</td>
+            <td>${this.company}</td>
+            <td>${this.dateApplied}</td>
+            <td>${this.link}</td>
+            <td>${this.status}</td>
+        `
+        return this.element
+    }
+
+    appendJob() {
+        const newRow = document.createElement('tr')
+        const filledRow = newRow.appendChild(this.jobHTML())
+        Job.jobContainer.appendChild(filledRow)
     }
 }
