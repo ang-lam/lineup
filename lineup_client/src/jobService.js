0b2ecc553756b64b1e385f361874943c3c50af25
Angela <angelalamluu@gmail.com>
2021-06-21 14:05:29 -0700

    created constructor for jobService

--- lineup_client/src/jobService.js
+++ lineup_client/src/jobService.js
@@ -1,1 +1,18 @@
-// responsible for fetch requests
+// responsible for fetch requests/ service calls
+
+class JobService{
+
+    constructor(endpoint){
+        this.endpoint = endpoint
+    }
+
+    // Read/Index action
+    
+    getJobs() {
+        fetch(`${this.endpoint}/jobs`)
+        .then(resp => resp.json())
+        .then(jobs => {
+            debugger
+        })
+    }
+}
