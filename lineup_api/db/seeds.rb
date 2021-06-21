467067a1dac2d722d4fadcb5ffad6d22bf4441d2
Angela <angelalamluu@gmail.com>
2021-06-21 13:33:49 -0700

    migrated tables and seeded data

--- lineup_api/db/seeds.rb
+++ lineup_api/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+first_job = Job.create(title: 'Entry Software Engineer', company: 'Microsoft', link: 'https://www.microsfot.com/careers/example', status: 'in-progress')
+
+Comment.create([
+    {description: 'Submit application by Saturday.', job: first_job},
+    {description: 'Resume objective needs to be updated for this position', job: first_job}
+])
