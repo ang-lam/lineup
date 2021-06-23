75711ed2ed109f4241ed6253f5f8320d6596a2a9
Angela <angelalamluu@gmail.com>
2021-06-22 23:28:32 -0700

    created function that initializes the headers of the table

--- lineup_client/index.js
+++ lineup_client/index.js
@@ -2,7 +2,7 @@ const baseUrl = 'http://127.0.0.1:3000/'
 const jobService = new JobService(baseUrl)
 
 Job.jobForm.addEventListener('submit', handleSubmit)
-
+initializeTable()
 jobService.getJobs()
 Job.renderForm()
 
@@ -11,4 +11,16 @@ function handleSubmit() {
     event.preventDefault()
     jobService.createJob()
     event.target.reset()
+}
+
+function initializeTable() {
+    Job.jobTable.innerHTML = `
+        <tr>
+            <th>Title</th>
+            <th>Company</th>
+            <th>Date Applied</th>
+            <th>Link</th>
+            <th>Status</th>
+        </tr>
+    `
 }
