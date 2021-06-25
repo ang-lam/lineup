e78a35974cbf71cd92813bffc83e1ffa3b797f22
Angela <angelalamluu@gmail.com>
2021-06-25 14:27:00 -0700

    added attributes to comment constructor

--- lineup_client/src/comment.js
+++ lineup_client/src/comment.js
@@ -3,6 +3,9 @@ class Comment{
     static all = []
 
     constructor(id, description, job_id){
+        this.id = id
+        this.description = description
+        this.job_id = job_id
         
         Comment.all.push(this)
     }
