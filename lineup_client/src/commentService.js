35bd755c90a5ab8948f9cd546b76200780cfc077
Angela <angelalamluu@gmail.com>
2021-06-29 16:49:50 -0700

    removed comments

--- lineup_client/src/commentService.js
+++ lineup_client/src/commentService.js
@@ -4,17 +4,7 @@ class CommentService{
     constructor(endpoint){
         this.endpoint = endpoint
     }
-
-    // getComments(job) {
-    //     fetch(`${this.endpoint}/comments`)
-    //         .then(resp => resp.json()
-    //         .then(comments => {
-    //             for (const comment of comments){
-    //                 const c = new Comment(comment)
-    //                 c.renderComments(job)
-    //             }
-    //     }))
-    // }
+    
     getComments() {
         fetch(`${this.endpoint}/comments`)
             .then(resp => resp.json()
