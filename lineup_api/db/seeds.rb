3a86f31419fb896be66bcf7aa4791511e05e6381
Angela <angelalamluu@gmail.com>
2021-07-02 15:36:13 -0700

    added more objects in seed file

--- lineup_api/db/seeds.rb
+++ lineup_api/db/seeds.rb
@@ -6,9 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-first_job = Job.create(title: 'Entry Software Engineer', company: 'Microsoft', link: 'https://www.microsfot.com/careers/example', status: 'in-progress')
+job1 = Job.create(title: 'Entry Software Engineer', company: 'Microsoft', date_applied: '', link: 'https://www.microsoft.com/careers/', status: 'In-progress')
+job6 = Job.create(title: 'Software Engineer', company: 'Google', date_applied: '', link: 'https://www.careers.google.com/', status: 'In-progress')
+job3 = Job.create(title: 'Software Development Engineer', company: 'Twitch', date_applied: '2021-06-30', link: 'https://www.careers.twitch.com/', status: 'Applied')
+job2 = Job.create(title: 'Software Engineer I', company: 'Zoom', date_applied: '2021-07-02', link: 'https://www.zoom.com/careers/', status: 'Applied')
+job5 = Job.create(title: 'Software Development Engineer', company: 'Amazon', date_applied: '', link: 'https://www.careers.amazon.com/', status: 'In-progress')
+job4 = Job.create(title: 'Software Engineer', company: 'Expedia', date_applied: '2021-06-14', link: 'https://www.expedia.com/careers/', status: 'Rejected')
+
 
 Comment.create([
-    {description: 'Submit application by Saturday.', job: first_job},
-    {description: 'Resume objective needs to be updated for this position', job: first_job}
+    {description: 'Submit application by Saturday', job: job1},
+    {description: 'Resume objective needs to be updated for this position', job: job1},
+    {description: 'Recruiter said need better project portfolio or experience for position', job: job4},
+    {description: 'Recruiter will reach out on Friday to give advice', job: job4},
+    {description: 'Reach out to people who work at Amazon for advice or referrals', job: job5},
+    {description: 'Resume objective needs to be updated for this position', job: job5},
+    {description: 'Submit application by Thursday.', job: job5},
+    {description: 'Resume objective needs to be updated for this position', job: job1},
+    {description: 'Interview on July 14th at 10AM', job: job3},
+    {description: 'Review interview questions', job: job3},
+    {description: 'Practice at least 10 leetcodes before interview', job: job3}
 ])
